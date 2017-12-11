3604563ee5eaeed9a1b7b1414bfe0a25a699e89b
Malou Kirstine Thale Vincents <maloukirstinethalevincents@Malous-MacBook-Pro-Retina.local>
2017-12-11 09:22:41 +0100

    hej

--- javascript.js
+++ javascript.js
@@ -1,12 +1,45 @@
-window.on("load", menu());
+//$(window).load(menu());
 
-function menu() {
-    console.log("test");
-    $(".bar_container").on("click", myFunction(this));
+//function menu() {
+//    console.log("menu");
+//    $(".dropdown").on("click", animeretMenu());
+//}
+//
+//function animeretMenu() {
+//    console.log("animeretMenu");
+//    $(".dropdown").off("click", animeretMenu());
+//
+//    /*$(".bar1").addClass("change_bar1");
+//    $(".bar2").addClass("change_bar2");
+//    $(".bar3").addClass("change_bar3");*/
+//
+//    $(".dropdown").on("click", dropdown());
+//}
+$(window).onload() {
+    klik();
 }
 
-function myFunction(x) {
-    console.log("test2");
+function klik(); {
+    $(".dropbtn").on("click", dropdown);
+}
 
-    x.classList.toggle("change");
+function dropdown() {
+    console.log("dropdown");
+
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
 }
