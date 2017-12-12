dad42055604e61a04d7eeea60bf8024fc4ea7241
Malou Kirstine Thale Vincents <maloukirstinethalevincents@Malous-MacBook-Pro-Retina.local>
2017-12-12 09:32:21 +0100

    dropdown menu

--- javascript.js
+++ javascript.js
@@ -1,45 +1,25 @@
-//$(window).load(menu());
+    $(".dropbtn").on("click", myFunction);
 
-//function menu() {
-//    console.log("menu");
-//    $(".dropdown").on("click", animeretMenu());
-//}
-//
-//function animeretMenu() {
-//    console.log("animeretMenu");
-//    $(".dropdown").off("click", animeretMenu());
-//
-//    /*$(".bar1").addClass("change_bar1");
-//    $(".bar2").addClass("change_bar2");
-//    $(".bar3").addClass("change_bar3");*/
-//
-//    $(".dropdown").on("click", dropdown());
-//}
-$(window).onload() {
-    klik();
-}
+    function myFunction() {
+        document.getElementById("myDropdown").classList.toggle("show");
+    }
 
-function klik(); {
-    $(".dropbtn").on("click", dropdown);
-}
 
-function dropdown() {
-    console.log("dropdown");
+    // Close the dropdown if the user clicks outside of it
+    window.onclick = function (event) {
+        if (!event.target.matches('.dropbtn')) {
 
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function (event) {
-    if (!event.target.matches('.dropbtn')) {
-
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
             }
         }
     }
-}
+
+    function myFunction1(x) {
+        x.classList.toggle("change");
+    }
